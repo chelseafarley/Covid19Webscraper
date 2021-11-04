92178f3c71ce77d99899916c50ea245c5f8cb25b
Chelsea Farley <chelseafarley91@gmail.com>
2021-11-04 13:58:09 +1300

    Day 4 - Webscraper and API in python

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# Web Scraping in Python
+
+This is an example of using python to do some webscraping. In this example, we scrape the covid nz website for locations of interest so we can display these on a map.
+
+
+## Install Dependices
+```bash
+  python3 -m pip install requests
+  python3 -m pip install beautifulsoup4
+  python3 -m pip install flask
+  python3 -m pip install gunicorn
+```
+
+## Running
+
+To run this project, change directories to this git repo and run the following command:
+
+```bash
+  python3 ./webscraping.py
+```
+
+This was also [deployed to Heroku](https://stackabuse.com/deploying-a-flask-application-to-heroku/).
---
+++ webscraping.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import sys
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+@app.route('/getlocations/', methods=['GET'])
+def respond():
+    url = "https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-health-advice-public/contact-tracing-covid-19/covid-19-contact-tracing-locations-interest"
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    locations_table = soup.find_all("table", class_="views-table")
+    row_locations = soup.find_all("tr", class_="odd")
+    row_locations.extend(soup.find_all("tr", class_="even"))
+
+    parsed_locations = {}
+    for row_location in row_locations:
+        name = row_location.find("td", class_="views-field-title").text.strip()
+        address = row_location.find("td", class_="views-field-field-address").text.strip()
+        day = row_location.find("td", class_="views-field-field-start-time").text.strip()
+        times = row_location.find("td", class_="views-field-field-finish-time").text.strip()
+
+        if name not in parsed_locations:
+            parsed_locations[name] = {
+                "name": name,
+                "address": address,
+                "exposure_events": []
+            }
+        
+        parsed_locations[name]["exposure_events"].append({
+            "day": day,
+            "times": times
+        })
+    
+    return Response(json.dumps(parsed_locations.values()), mimetype="application/json")
+
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
